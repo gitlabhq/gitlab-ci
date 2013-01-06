96aac5e89fd1c9da89e17552dabd9653489f5a0a
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2013-01-06 20:21:34 +0200

    Remove resque_dev -> use foreman instead

--- resque_dev.sh
+++
@@ -1,3 +0,0 @@
-mkdir -p tmp/pids
-nohup bundle exec rake environment resque:work QUEUE=runner,scheduler_task VVERBOSE=1 RAILS_ENV=development PIDFILE=tmp/pids/resque_worker.pid > ./log/resque.log  &
-nohup bundle exec rake environment resque:scheduler VVERBOSE=1 RAILS_ENV=development PIDFILE=tmp/pids/resque_schedule.pid > ./log/schedule.log  &
