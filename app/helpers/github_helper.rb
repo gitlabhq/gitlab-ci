9db4003a28ebf278ba28e1592a7b6296fdeb16fa
Dmitry Galinsky <dima.exe@gmail.com>
2013-02-24 00:14:15 +0400

    [github] fixes links to github

--- app/helpers/github_helper.rb
+++ app/helpers/github_helper.rb
@@ -6,10 +6,12 @@ module GithubHelper
   def link_to_github_build(build)
     link = "https://github.com/#{build.project.name}/commit/#{build.sha}"
     title = github_build_title(build)
+    ico = "icon-arrow-right"
     unless build.pull_request_number.blank?
       link = "https://github.com/#{build.project.name}/pull/#{build.pull_request_number}/files"
+      ico = "icon-share-alt"
     end
-    link_to title, link
+    raw("#{title} " + link_to(%{ <i class="#{ico}"></i> }.html_safe, link))
   end
 
   def github_build_title(build)
