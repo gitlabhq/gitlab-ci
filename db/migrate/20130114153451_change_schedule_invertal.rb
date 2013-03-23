0f9de8791e9f5ca6392e5ffc02989e3c7bcae4e5
dn <dn@wortbit.de>
2013-03-23 08:14:39 +0100

    There is no implicit cast from varchar to integer.
    PG::Error: ERROR:  column "number" cannot be cast to type integer.

--- db/migrate/20130114153451_change_schedule_invertal.rb
+++ db/migrate/20130114153451_change_schedule_invertal.rb
@@ -1,9 +1,25 @@
 class ChangeScheduleInvertal < ActiveRecord::Migration
   def up
-    change_column :projects, :polling_interval, :integer, null: true
+    if ActiveRecord::Base.connection.instance_of?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
+      connection.execute(%q{
+        alter table projects
+        alter column polling_interval
+        type integer using cast(polling_interval as integer)
+      }) 
+    else
+      change_column :projects, :polling_interval, :integer, null: true
+    end
   end
 
   def down
-    change_column :projects, :polling_interval, :string, null: true
+    if ActiveRecord::Base.connection.instance_of?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
+      connection.execute(%q{
+        alter table projects
+        alter column polling_interval
+        type varchar using cast(polling_interval as varchar)
+      }) 
+    else
+      change_column :projects, :polling_interval, :string, null: true
+    end
   end
 end
