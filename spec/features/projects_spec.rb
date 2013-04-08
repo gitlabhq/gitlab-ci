eec2e177d74be20e4444ef30e210464d7d5928b2
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2013-04-08 22:56:30 +0300

    more project feature specs

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -23,5 +23,32 @@ describe "Projects" do
     it { page.should have_content @project.name }
     it { page.should have_content 'All builds' }
   end
+
+  describe "GET /projects/:id/edit" do
+    before do
+      visit edit_project_path(@project)
+    end
+
+    it { page.should have_content @project.name }
+    it { page.should have_content 'Build Schedule' }
+  end
+
+  describe "GET /projects/:id/stats" do
+    before do
+      visit stats_project_path(@project)
+    end
+
+    it { page.should have_content @project.name }
+    it { page.should have_content 'Some stats related to the project' }
+  end
+
+  describe "GET /projects/:id/details" do
+    before do
+      visit details_project_path(@project)
+    end
+
+    it { page.should have_content @project.name }
+    it { page.should have_content 'Integration with GitLab and other services' }
+  end
 end
 
