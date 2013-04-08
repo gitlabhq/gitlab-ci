4ca0bd3c78bb5e0e1b0ebfee31365ee10c16f277
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2013-04-08 22:35:10 +0300

    Fix user profile update, show user gravatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,9 +16,20 @@ class UsersController < ApplicationController
   end
 
   def update
-    current_user.update_attributes(params[:user])
+    user_params = params[:user]
 
-    redirect_to :back
+    if current_user.valid_password?(user_params.delete(:current_password))
+
+      user_params.delete(:password) if user_params[:password].blank?
+      user_params.delete(:password_confirmation) if user_params[:password_confirmation].blank?
+
+      current_user.update_attributes(user_params)
+
+      redirect_to :back
+    else
+
+      redirect_to :back, alert: 'Current password is invalid'
+    end
   end
 
   def destroy
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def loader_html
     image_tag 'loader.gif'
   end
+
+  def gravatar_icon(user_email = '', size = 40)
+    gravatar_url = 'http://www.gravatar.com/avatar/%{hash}?s=%{size}&d=mm'
+    user_email.strip!
+    sprintf gravatar_url, hash: Digest::MD5.hexdigest(user_email.downcase), size: size
+  end
 end
