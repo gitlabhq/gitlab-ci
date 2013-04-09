cb9d56e09d0e2f89db294780aa67431ce863ee84
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2013-04-09 09:22:47 +0300

    Build feature specs

--- spec/factories/builds.rb
+++ spec/factories/builds.rb
@@ -3,5 +3,6 @@
 FactoryGirl.define do
   factory :build do
     ref 'master'
+    sha '97de212e80737a608d939f648d959671fb0a0142'
   end
 end
--- spec/features/builds_spec.rb
+++ spec/features/builds_spec.rb
@@ -1,7 +1,19 @@
 require 'spec_helper'
 
 describe "Builds" do
-  describe "GET /:project/builds" do
+  before do
+    login_as :user
+    @project = FactoryGirl.create :project
+    @build = FactoryGirl.create :build, project: @project
+  end
 
+  describe "GET /:project/builds" do
+    before do
+      visit project_build_path(@project, @build)
+    end
+
+    it { page.should have_content @build.sha[0..7] }
+    it { page.should have_content @build.git_commit_message }
+    it { page.should have_content @build.git_author_name }
   end
 end
