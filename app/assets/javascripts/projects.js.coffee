0378340a6fb7a48e55355b2d61708811ee9a9223
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2013-04-09 10:08:32 +0300

    build badge status markdown a& html code for user

---
+++ app/assets/javascripts/projects.js.coffee
@@ -0,0 +1,3 @@
+$(document).ready ->
+  $('.badge-codes-toggle').on 'click', ->
+    $('.badge-codes-block').toggle()
--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -32,4 +32,14 @@ module ProjectsHelper
     ratio = (success_builds.to_f / (success_builds + failed_builds)) * 100
     ratio.to_i
   end
+
+  def markdown_badge_code(project, ref)
+    url = status_project_url(project, ref: ref, format: 'png')
+    "[![build status](#{url})](#{project_url(project, ref: ref)})"
+  end
+
+  def html_badge_code(project, ref)
+    url = status_project_url(project, ref: ref, format: 'png')
+    "<a href='#{project_url(project, ref: ref)}'><img src='#{url}' /></a>"
+  end
 end
