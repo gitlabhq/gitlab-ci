48a7373b9e490522bd98e60f42af45cee9b7a299
Ingo Gottwald <i.gottwald@tomorrow-focus.de>
2013-04-24 14:39:02 +0200

    Serving gravatar images form an https source This is safe to run on gitlab-ci http and https servers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
   end
 
   def gravatar_icon(user_email = '', size = 40)
-    gravatar_url = 'http://www.gravatar.com/avatar/%{hash}?s=%{size}&d=mm'
+    gravatar_url = 'https://www.gravatar.com/avatar/%{hash}?s=%{size}&d=mm'
     user_email.strip!
     sprintf gravatar_url, hash: Digest::MD5.hexdigest(user_email.downcase), size: size
   end
