2af7985d5d0399c0299eafcac3e4ce8a6f2ac342
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2013-07-05 20:57:58 +0300

    Add description filed to runner

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130628142321) do
+ActiveRecord::Schema.define(:version => 20130705171042) do
 
   create_table "builds", :force => true do |t|
     t.integer  "project_id"
@@ -57,8 +57,9 @@ ActiveRecord::Schema.define(:version => 20130628142321) do
   create_table "runners", :force => true do |t|
     t.string   "token"
     t.text     "public_key"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.string   "description"
   end
 
 end
