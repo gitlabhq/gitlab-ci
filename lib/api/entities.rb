cd9d7dd8701d916842034d3d2f0f687f88157bdf
Nick Huanca <nick.huanca@gmail.com>
2013-09-03 21:35:57 -0600

    adding before commit to available info for build.
    adding before commit to available info for build. will help with diff for using like post-receive hook (oldrev / newrev)

--- lib/api/entities.rb
+++ lib/api/entities.rb
@@ -1,7 +1,7 @@
 module API
   module Entities
     class Build < Grape::Entity
-      expose :id, :commands, :path, :ref, :sha, :project_id, :repo_url
+      expose :id, :commands, :path, :ref, :sha, :project_id, :repo_url, :before_sha
     end
 
     class Runner < Grape::Entity
