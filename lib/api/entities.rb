0373823ad909f9121ad52e7ff5f4031d56611a2e
gdunkle <galendunkleberger@gmail.com>
2013-10-22 16:11:38 -0400

    add project name

--- lib/api/entities.rb
+++ lib/api/entities.rb
@@ -1,7 +1,7 @@
 module API
   module Entities
     class Build < Grape::Entity
-      expose :id, :commands, :path, :ref, :sha, :project_id, :repo_url
+      expose :id, :commands, :path, :ref, :sha, :project_id, :repo_url, :project_name
     end
 
     class Runner < Grape::Entity
