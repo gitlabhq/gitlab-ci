7e348ca2260ae422ecc333dbb8b81dfcc6e78fbf
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2014-01-30 12:32:42 +0200

    Update db schema
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -9,75 +9,75 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131120155545) do
+ActiveRecord::Schema.define(version: 20131120155545) do
 
-  create_table "builds", :force => true do |t|
+  create_table "builds", force: true do |t|
     t.integer  "project_id"
     t.string   "ref"
     t.string   "status"
     t.datetime "finished_at"
-    t.text     "trace",       :limit => 1073741823
-    t.datetime "created_at",                        :null => false
-    t.datetime "updated_at",                        :null => false
+    t.text     "trace",       limit: 2147483647
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "sha"
     t.datetime "started_at"
     t.string   "tmp_file"
     t.string   "before_sha"
-    t.text     "push_data"
+    t.text     "push_data",   limit: 16777215
     t.integer  "runner_id"
   end
 
-  add_index "builds", ["project_id"], :name => "index_builds_on_project_id"
-  add_index "builds", ["runner_id"], :name => "index_builds_on_runner_id"
+  add_index "builds", ["project_id"], name: "index_builds_on_project_id", using: :btree
+  add_index "builds", ["runner_id"], name: "index_builds_on_runner_id", using: :btree
 
-  create_table "projects", :force => true do |t|
-    t.string   "name",                                       :null => false
-    t.integer  "timeout",                 :default => 1800,  :null => false
-    t.text     "scripts",                                    :null => false
-    t.datetime "created_at",                                 :null => false
-    t.datetime "updated_at",                                 :null => false
+  create_table "projects", force: true do |t|
+    t.string   "name",                                    null: false
+    t.integer  "timeout",                 default: 1800,  null: false
+    t.text     "scripts",                                 null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "token"
     t.string   "default_ref"
     t.string   "gitlab_url"
-    t.boolean  "always_build",            :default => false, :null => false
+    t.boolean  "always_build",            default: false, null: false
     t.integer  "polling_interval"
-    t.boolean  "public",                  :default => false, :null => false
+    t.boolean  "public",                  default: false, null: false
     t.string   "ssh_url_to_repo"
     t.integer  "gitlab_id"
-    t.boolean  "allow_git_fetch",         :default => true,  :null => false
-    t.string   "email_recipients",        :default => "",    :null => false
-    t.boolean  "email_add_committer",     :default => true,  :null => false
-    t.boolean  "email_all_broken_builds", :default => true,  :null => false
+    t.boolean  "allow_git_fetch",         default: true,  null: false
+    t.string   "email_recipients",        default: "",    null: false
+    t.boolean  "email_add_committer",     default: true,  null: false
+    t.boolean  "email_all_broken_builds", default: true,  null: false
   end
 
-  create_table "runner_projects", :force => true do |t|
-    t.integer  "runner_id",  :null => false
-    t.integer  "project_id", :null => false
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  create_table "runner_projects", force: true do |t|
+    t.integer  "runner_id",  null: false
+    t.integer  "project_id", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "runner_projects", ["project_id"], :name => "index_runner_projects_on_project_id"
-  add_index "runner_projects", ["runner_id"], :name => "index_runner_projects_on_runner_id"
+  add_index "runner_projects", ["project_id"], name: "index_runner_projects_on_project_id", using: :btree
+  add_index "runner_projects", ["runner_id"], name: "index_runner_projects_on_runner_id", using: :btree
 
-  create_table "runners", :force => true do |t|
+  create_table "runners", force: true do |t|
     t.string   "token"
     t.text     "public_key"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "description"
   end
 
-  create_table "sessions", :force => true do |t|
-    t.string   "session_id", :null => false
+  create_table "sessions", force: true do |t|
+    t.string   "session_id", null: false
     t.text     "data"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
-  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
+  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", using: :btree
+  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
 
 end
