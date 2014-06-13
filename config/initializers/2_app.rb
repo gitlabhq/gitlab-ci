e5d14c89a69e66a7f8615149ff8ddffe03cf55e0
Sytse Sijbrandij <sytses@gmail.com>
2014-06-13 17:13:00 +0200

    Remove added file and rename runners token constant to registration token.

--- config/initializers/2_app.rb
+++ config/initializers/2_app.rb
@@ -1,7 +1,7 @@
 module GitlabCi
   VERSION = File.read(Rails.root.join("VERSION")).strip
   REVISION = `git log --pretty=format:'%h' -n 1`
-  RUNNERS_TOKEN = SecureRandom.hex(10)
+  REGISTRATION_TOKEN = SecureRandom.hex(10)
 
   def self.config
     Settings
