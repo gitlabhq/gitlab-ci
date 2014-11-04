c21288c06bc4e7ed22c301f118aa1c14cda85c9d
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2014-11-04 19:25:01 +0200

    Add more test coverage
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- spec/features/admin/projects_spec.rb
+++ spec/features/admin/projects_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "Admin Projects" do
+  let(:project) { FactoryGirl.create :project }
+
   before do
     skip_admin_auth
     login_as :user
@@ -13,5 +15,13 @@ describe "Admin Projects" do
 
     it { page.should have_content "Manage Projects" }
   end
-end
 
+  describe "GET /admin/projects/:id" do
+    before do
+      visit admin_project_path(project)
+    end
+
+    it { page.should have_content "Project info" }
+    it { page.should have_content project.name }
+  end
+end
--- spec/features/builds_spec.rb
+++ spec/features/builds_spec.rb
@@ -17,4 +17,25 @@ describe "Builds" do
     it { page.should have_content @commit.git_commit_message }
     it { page.should have_content @commit.git_author_name }
   end
+
+  describe "GET /:project/builds/:id/cancel" do
+    before do
+      @build.run!
+      visit cancel_project_build_path(@project, @build)
+    end
+
+    it { page.should have_content 'canceled' }
+    it { page.should have_content 'Retry' }
+  end
+
+  describe "POST /:project/builds/:id/retry" do
+    before do
+      @build.cancel!
+      visit project_build_path(@project, @build)
+      click_link 'Retry'
+    end
+
+    it { page.should have_content 'pending' }
+    it { page.should have_content 'Cancel' }
+  end
 end
