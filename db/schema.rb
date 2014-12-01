f005f04ec482d27ccddbfeb116280678cfc41219
Valery Sizov <valery@gitlab.com>
2014-12-01 06:40:31 -0500

    enable pgplgsql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141201153755) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "builds", force: true do |t|
     t.integer  "project_id"
     t.string   "ref"
