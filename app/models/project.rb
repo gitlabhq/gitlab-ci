16be7c83d5bb4db501daadea896a3784cc2b4855
Stefan Kirrmann <kirrmann@users.noreply.github.com>
2014-12-03 14:48:06 +0100

    Add missing last_build to project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -38,6 +38,7 @@ class Project < ActiveRecord::Base
   has_many :runners, through: :runner_projects
   has_many :web_hooks, dependent: :destroy
   has_many :jobs, dependent: :destroy
+  has_one :last_build, ->() { order('id DESC') }, class_name: 'Build'
 
   accepts_nested_attributes_for :jobs, allow_destroy: true
 
