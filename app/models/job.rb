188f952f248f756b625adcdc4a5c251d74a1acd2
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2015-01-12 00:16:32 -0800

    Annotate

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,13 +2,15 @@
 #
 # Table name: jobs
 #
-#  id         :integer          not null, primary key
-#  project_id :integer          not null
-#  commands   :text
-#  active     :boolean          default(TRUE), not null
-#  created_at :datetime
-#  updated_at :datetime
-#  name       :string(255)
+#  id             :integer          not null, primary key
+#  project_id     :integer          not null
+#  commands       :text
+#  active         :boolean          default(TRUE), not null
+#  created_at     :datetime
+#  updated_at     :datetime
+#  name           :string(255)
+#  build_branches :boolean          default(TRUE), not null
+#  build_tags     :boolean          default(FALSE), not null
 #
 
 class Job < ActiveRecord::Base
