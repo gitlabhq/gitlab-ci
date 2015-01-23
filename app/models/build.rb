ba1a96df317eac0848beb7bc591b4897643cd012
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2015-01-23 14:10:09 -0800

    Fix Build#trace

--- app/models/build.rb
+++ app/models/build.rb
@@ -146,7 +146,7 @@ class Build < ActiveRecord::Base
   end
 
   def trace
-    if project
+    if project && read_attribute(:trace).present?
       read_attribute(:trace).gsub(project.token, 'xxxxxx')
     end
   end
