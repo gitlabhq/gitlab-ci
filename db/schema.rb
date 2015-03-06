0c71a4c41465cd620e163e081f31567ce59de766
Kamil Trzcinski <ayufan@ayufan.eu>
2015-03-06 14:29:11 +0100

    Added runner last contact

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150226001835) do
+ActiveRecord::Schema.define(version: 20150306131416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -108,6 +108,7 @@ ActiveRecord::Schema.define(version: 20150226001835) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "description"
+    t.datetime "contacted_at"
   end
 
   create_table "services", force: true do |t|
