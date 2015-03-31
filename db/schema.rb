9437042282b098b25982e8bff6a3d4d156b9a28f
Valery Sizov <vsv2711@gmail.com>
2015-03-31 15:04:03 +0300

    Disable running on shared runners

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150324001227) do
+ActiveRecord::Schema.define(version: 20150330001111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
