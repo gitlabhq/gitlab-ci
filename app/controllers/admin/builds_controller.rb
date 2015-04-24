b98d2e9958d4c01ecf93c0abe44c51029033b3c1
Valery Sizov <vsv2711@gmail.com>
2015-04-24 14:54:16 +0300

    Tabs Pending and Running on admin builds page

--- app/controllers/admin/builds_controller.rb
+++ app/controllers/admin/builds_controller.rb
@@ -1,5 +1,10 @@
 class Admin::BuildsController < Admin::ApplicationController
   def index
+    @scope = params[:scope]
     @builds = Build.order('created_at DESC').page(params[:page]).per(30)
+
+    if ["pending", "running"].include? @scope
+      @builds = @builds.send(@scope)
+    end
   end
 end
