a3b71d6d1519477940d303c5b74cfa3685086407
MOZGIII <mike-n@narod.ru>
2015-05-24 17:30:36 +0300

    Fixed issue with limits_to_mysql blocking fresh install
    Guess this was lost in migrations.
    It used to block fresh `rake setup`.

--- db/migrate/limits_to_mysql.rb
+++ db/migrate/limits_to_mysql.rb
@@ -3,6 +3,6 @@ class LimitsToMysql < ActiveRecord::Migration
     return unless ActiveRecord::Base.configurations[Rails.env]['adapter'] =~ /^mysql/
 
     change_column :builds, :trace, :text, limit: 1073741823
-    change_column :builds, :push_data, :text, limit: 16777215
+    change_column :commits, :push_data, :text, limit: 16777215
   end
 end
