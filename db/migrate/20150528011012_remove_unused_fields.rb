0cb18461890ecf514cab9ea4e7b236eeae3bef0d
Valery Sizov <vsv2711@gmail.com>
2015-06-03 19:19:08 +0300

    migrate job names

--- db/migrate/20150528011012_remove_unused_fields.rb
+++ db/migrate/20150528011012_remove_unused_fields.rb
@@ -1,5 +1,13 @@
 class RemoveUnusedFields < ActiveRecord::Migration
-  def change
+  def up
+    select_all("SELECT id, name FROM jobs").each do |job|
+      execute("UPDATE builds SET name = '#{quote_string(job["name"])}' WHERE job_id = #{job["id"]}")
+    end
+
     remove_column :builds, :job_id, :integer
+  end
+
+  def down
+    add_column :builds, :job_id, :integer
   end
 end
