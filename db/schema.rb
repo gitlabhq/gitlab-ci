1c7bb9d51c775bd43a3334786d01685f4ce2ade0
Valery Sizov <vsv2711@gmail.com>
2015-06-03 13:34:01 +0300

    increase default build timeout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150508011360) do
+ActiveRecord::Schema.define(version: 20150602000240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,7 +82,7 @@ ActiveRecord::Schema.define(version: 20150508011360) do
 
   create_table "projects", force: true do |t|
     t.string   "name",                                     null: false
-    t.integer  "timeout",                  default: 1800,  null: false
+    t.integer  "timeout",                  default: 3600,  null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "token"
