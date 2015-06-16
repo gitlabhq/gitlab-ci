40830fee8bcdfd465fb87fada28ef049a080acf2
Valery Sizov <vsv2711@gmail.com>
2015-06-16 15:12:36 +0300

    better yaml validation

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -93,9 +93,19 @@ class Commit < ActiveRecord::Base
 
   def create_builds
     return if skip_ci?
-    return unless config_processor.valid?
 
-    config_processor.builds_for_ref(ref, tag).each do |build_attrs|
+    unless config_processor.valid?
+      save_yaml_error(config_processor.errors.join(",")) and return
+    end
+
+    begin
+      builds_for_ref = config_processor.builds_for_ref(ref, tag)
+    rescue Exception => e
+      logger.error e.message + "\n" + e.backtrace.join("\n")
+      save_yaml_error("Undefined yaml error") and return
+    end
+
+    builds_for_ref.each do |build_attrs|
       builds.create!({
         project: project,
         name: build_attrs[:name],
@@ -121,9 +131,19 @@ class Commit < ActiveRecord::Base
 
   def create_deploy_builds
     return if skip_ci?
-    return unless config_processor.valid?
 
-    config_processor.deploy_builds_for_ref(ref, tag).each do |build_attrs|
+    unless config_processor.valid?
+      save_yaml_error(config_processor.errors.join(",")) and return
+    end
+
+    begin
+      deploy_builds_for_ref = config_processor.deploy_builds_for_ref(ref, tag)
+    rescue Exception => e
+      logger.error e.message + "\n" + e.backtrace.join("\n")
+      save_yaml_error("Undefined yaml error") and return
+    end
+
+    deploy_builds_for_ref.each do |build_attrs|
       builds.create!({
         project: project,
         name: build_attrs[:name],
@@ -135,6 +155,10 @@ class Commit < ActiveRecord::Base
   end
 
   def status
+    if yaml_errors.present?
+      return 'failed'
+    end
+
     if success?
       'success'
     elsif pending?
@@ -206,5 +230,12 @@ class Commit < ActiveRecord::Base
     else
       false
     end
+  end
+
+  private
+
+  def save_yaml_error(error)
+    self.yaml_errors = error
+    save
   end
 end
